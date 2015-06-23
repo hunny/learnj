8c5220581825e183498e6576f60c4d37828c8a91
hunnyhu <huxiong888@163.com>
2015-06-23 22:23:27 +0800

    Interpreter Design Pattern Comment.

--- src/main/java/hh/learnj/designpattern/interpreter/step3/InterpreterPatternDemo.java
+++ src/main/java/hh/learnj/designpattern/interpreter/step3/InterpreterPatternDemo.java
@@ -5,8 +5,29 @@ import hh.learnj.designpattern.interpreter.step2.AndExpression;
 import hh.learnj.designpattern.interpreter.step2.OrExpression;
 import hh.learnj.designpattern.interpreter.step2.TerminalExpression;
 
+/**
+ * Interpreter pattern provides a way to evaluate language grammar or
+ * expression. This type of pattern comes under behavioral pattern. This pattern
+ * involves implementing an expression interface which tells to interpret a
+ * particular context. This pattern is used in SQL parsing, symbol processing
+ * engine etc.
+ * 
+ * Implementation We are going to create an interface Expression and concrete
+ * classes implementing the Expression interface. A class TerminalExpression is
+ * defined which acts as a main interpreter of context in question. Other
+ * classes OrExpression, AndExpression are used to create combinational
+ * expressions.
+ * 
+ * InterpreterPatternDemo, our demo class, will use Expression class to create
+ * rules and demonstrate parsing of expressions.
+ * 
+ * http://www.tutorialspoint.com/design_pattern/interpreter_pattern.htm
+ * 
+ * @author hunnyhu
+ *
+ */
 public class InterpreterPatternDemo {
-	
+
 	// Rule: Robert and John are male
 	public static Expression getMaleExpression() {
 		Expression robert = new TerminalExpression("Robert");
@@ -22,7 +43,7 @@ public class InterpreterPatternDemo {
 	}
 
 	public static void main(String[] args) {
-		
+
 		Expression isMale = getMaleExpression();
 		Expression isMarriedWoman = getMarriedWomanExpression();
 
