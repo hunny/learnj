a2056f653c8f84d6710bd6e8a6680f1f80f3e314
hunnyhu <huxiong888@163.com>
2016-03-15 23:53:48 +0800

    Add Thread notify Mode.

--- src/main/java/hh/learnj/enhance/thread/TraditionalThreadCommunicateTest.java
+++ src/main/java/hh/learnj/enhance/thread/TraditionalThreadCommunicateTest.java
@@ -3,10 +3,10 @@ package hh.learnj.enhance.thread;
 public class TraditionalThreadCommunicateTest {
 
 	public static void main(String[] args) {
-		testWithStaticClassLevel();
+		// testWithStaticClassLevel();
 		tstWithClassLevel();
 	}
-	
+
 	public static void tstWithClassLevel() {
 
 		final Business business = new Business();
@@ -14,64 +14,81 @@ public class TraditionalThreadCommunicateTest {
 
 			@Override
 			public void run() {
-				synchronized(TraditionalThreadCommunicateTest.class) {
-					for (int i = 0; i < 10; i++) {
-						business.sub(i);
-					}
+				for (int i = 0; i < 10; i++) {
+					business.sub(i);
 				}
 			}
-			
+
 		}).start();
 
-		synchronized(TraditionalThreadCommunicateTest.class) {
-			for (int i = 0; i < 10; i++) {
-				business.main(i);
-			}
+		for (int i = 0; i < 10; i++) {
+			business.main(i);
 		}
 	}
-	
+
 	public static void testWithStaticClassLevel() {
 		new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				synchronized(TraditionalThreadCommunicateTest.class) {
+				synchronized (TraditionalThreadCommunicateTest.class) {
 					for (int i = 0; i < 10; i++) {
 						for (int j = 0; j < 20; j++) {
-							System.out.println("static level sub thread sequence of " + j
-									+ ", loop at " + i);
+							System.out
+									.println("static level sub thread sequence of "
+											+ j + ", loop at " + i);
 						}
 					}
 				}
 			}
-			
+
 		}).start();
 
-		synchronized(TraditionalThreadCommunicateTest.class) {
+		synchronized (TraditionalThreadCommunicateTest.class) {
 			for (int i = 0; i < 10; i++) {
 				for (int j = 0; j < 30; j++) {
-					System.out.println("static level main thread sequence of " + j
-							+ ", loop at " + i);
+					System.out.println("static level main thread sequence of "
+							+ j + ", loop at " + i);
 				}
 			}
 		}
 	}
-	
+
 }
 
 class Business {
-	
+
+	private boolean shouldSub = true;
+
 	public synchronized void sub(int i) {
+		while (!shouldSub) {// 如果不是子线程，需要启动等待模式
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		for (int j = 0; j < 20; j++) {
 			System.out.println("class level sub thread sequence of " + j
 					+ ", loop at " + i);
 		}
+		shouldSub = false;
+		this.notify();
 	}
-	
+
 	public synchronized void main(int i) {
+		while (shouldSub) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		for (int j = 0; j < 30; j++) {
 			System.out.println("class level main thread sequence of " + j
 					+ ", loop at " + i);
 		}
+		shouldSub = true;
+		this.notify();
 	}
 }
