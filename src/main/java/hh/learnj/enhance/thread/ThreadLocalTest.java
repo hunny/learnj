a41aaa4d55f43e50ef5ddad10a2106b9254dcff6
hunnyhu <huxiong888@163.com>
2016-03-16 23:32:37 +0800

    Thread Local Test.

--- src/main/java/hh/learnj/enhance/thread/ThreadLocalTest.java
+++ src/main/java/hh/learnj/enhance/thread/ThreadLocalTest.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class ThreadLocalTest {
 
 	public static void main(String[] args) {
-
 		for (int i = 0; i < 10; i++) {
 			new Thread(new Runnable() {
 				@Override
