3ff623db63f07118f7770b77fb2f1277aac5d280
hunnyhu <huxiong888@163.com>
2017-03-27 22:36:16 +0800

    topease

---
+++ src/main/java/hh/learnj/httpclient/usecase/topease/topease.sql
@@ -0,0 +1,4 @@
+-- Base on qichacha.sql
+ALTER TABLE ADD COLUMN mobile VARCHAR(254); /* 客户状态 */
+ALTER TABLE ADD COLUMN saleman VARCHAR(254); /* 销售代表 */
+ALTER TABLE ADD COLUMN serviceman VARCHAR(254); /* 服务代表 */
